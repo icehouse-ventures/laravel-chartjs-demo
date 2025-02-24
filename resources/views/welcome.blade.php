7e570d5793b4759bb2e02f1d53c640a42c8c4069
Peter Thomson <p.thomson@theicehouse.co.nz>
2025-02-25 10:03:00 +1300

    Bump dependancies and prepare for Laravel Cloud.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,24 @@
                     </div>
                 </div>
 
+                <div class="mt-16">
+                    <div class="">
+                        <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                            <div>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel ChartJS Package Links</h2>
+
+                                <ul class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed" style="list-style: disc; padding: 0 0 0 24px;">
+                                    <li><a href="https://github.com/icehouse-ventures/laravel-chartjs-demo">Laravel ChartJS Demo GitHub Repository</a></li>
+                                    <li><a href="https://github.com/icehouse-ventures/laravel-chartjs">Laravel ChartJS Package GitHub Repository</a></li>
+                                    <li><a href="https://packagist.org/packages/icehouse-ventures/laravel-chartjs">Laravel ChartJS Packagist for Composer</a></li>
+                                    <li><a href="https://github.com/icehouse-ventures/laravel-chartjs/wiki">Laravel ChartJS Wiki</a></li>
+                                    <li><a href="https://icehouse-ventures.github.io/laravel-chartjs/">Laravel ChartJS Package Documentation</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
